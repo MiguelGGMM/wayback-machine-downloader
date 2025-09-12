a21bf5772b2b35a01ebf9e1dededd6690cd8c1f2
MiguelGGMM <104460442+MiguelGGMM@users.noreply.github.com>
2025-09-12 18:44:48 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// eslint.config.mjs
+import js from '@eslint/js';
+import * as parser from '@typescript-eslint/parser';
+import pluginTs from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  //js.configs.recommended,
+  { ...js.configs.recommended, files: ['**/*.js', '**/*.mjs', '**/*.cjs'] },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],        // enable type-aware rules (optional but nice)
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': pluginTs,
+    },
+    rules: {
+      // turn on some TS rules; add more as you like
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-misused-promises': ['warn', { checksVoidReturn: { attributes: false } }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@/no-duplicate-imports': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+];
